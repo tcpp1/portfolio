55ec08592f3620e21d6e02588fb684723ee266c9
tcpp1 <78777075+tcpp1@users.noreply.github.com>
2021-06-22 14:12:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,29 @@ $(function(){
 
 	$('.main-title').fadeIn(4000);
 
+
+
+		$('.langbtn').click(function(){
+			if($('.eng').hasClass('active2')){
+				$('.eng').removeClass('active2');
+				$('.jpn').addClass('active2');
+			}else{
+				$('.jpn').removeClass('active2');
+				$('.eng').addClass('active2');
+			};
+		});
+
+
+
 	$('.Teethumb').click(function(){
 			$('.active').removeClass('active');
-
 			var clickedIndex = $('.Teethumb').index($(this));
 
 			$('.slide').eq(clickedIndex).addClass('active');
-
-		});
+			$('.slide').eq(clickedIndex).addClass('animate__fadeInRight');
 
 	});
 
+
+
+});
